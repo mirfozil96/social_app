7832c3e302d4518fe79521f8381b9ff7f1c6bbcf
mirfozil <99140314+mirfozil96@users.noreply.github.com>
2024-07-09 16:49:24 +0500

    first commit

---
+++ lib/main_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:social_app/features/home_page/home_page.dart';
+
+import 'package:social_app/features/notification_page/notification_page.dart';
+
+import 'package:social_app/nav_bar_widget.dart';
+import 'package:social_app/features/profile_page/profile_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int selectedIndex = 0;
+
+  final ValueNotifier<int> _selectedIndexNotifier = ValueNotifier(0);
+  final controller = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedIndexNotifier.addListener(() {
+      controller.jumpToPage(_selectedIndexNotifier.value);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          itemCount: 5,
+          controller: controller,
+          onPageChanged: (index) => _selectedIndexNotifier.value = index,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return const HomePage();
+            }
+            if (index == 1) {
+              return const NotificationPage();
+            }
+            if (index == 2) {
+              return const NotificationPage();
+            }
+            if (index == 3) {
+              return const NotificationPage();
+            }
+            if (index == 4) {
+              return const ProfilePage();
+            }
+
+            return Container();
+          }),
+      bottomNavigationBar: ValueListenableBuilder(
+          valueListenable: _selectedIndexNotifier,
+          builder: (context, int value, child) {
+            return NavBarWidget(
+              currentIndex: value,
+              onIndexChange: (index) => _selectedIndexNotifier.value = index,
+            );
+          }),
+    );
+  }
+
+  @override
+  void dispose() {
+    _selectedIndexNotifier.dispose();
+    super.dispose();
+  }
+}
